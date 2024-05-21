1cafd0eab080402050ad86e086129b83e7539c2c
Karl George <karlgeorge87@gmail.com>
2024-05-21 13:34:03 +0200

    Style sign up page

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,11 +1,92 @@
-import { View, Text } from 'react-native';
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { Link } from 'expo-router';
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
 const Page = () => {
+  const [countryCode, setCountryCode] = useState('+49');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  // Could use this to check for IOS or ANDROID
+  // const keyboardVerticalOffset = Platform.OS === 'ios' ? 80 : 80;
+
+  const onSignup = async () => {};
+
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
+    <KeyboardAvoidingView style={{ flex: 1 }}>
+      <View style={defaultStyles.container}>
+        <Text style={defaultStyles.header}>Let's get started!</Text>
+        <Text style={defaultStyles.descriptionText}>
+          Enter your phone number. We will send a confirmation code to your
+          device
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholderTextColor={Colors.gray}
+            value={countryCode}
+          />
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder='Mobile number'
+            placeholderTextColor={Colors.gray}
+            keyboardType='numeric'
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+        </View>
+        <Link href={'/login'} replace asChild>
+          <TouchableOpacity>
+            <Text style={defaultStyles.textLink}>
+              Already have an account? Log in
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+        <View style={{ flex: 1 }} />
+
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButton,
+            phoneNumber !== '' ? styles.enabled : styles.disabled,
+            { marginBottom: 20 },
+          ]}
+          onPress={onSignup}
+        >
+          <Text style={defaultStyles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: 'row',
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
 
 export default Page;
