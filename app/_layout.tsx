29b374a983830079c47f93d8f59362919d60b535
Karl George <karlgeorge87@gmail.com>
2024-05-22 11:33:12 +0200

    Add auth checks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,12 +3,12 @@ import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
 import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Link, Stack, useRouter } from 'expo-router';
+import { Link, Stack, useRouter, useSegments } from 'expo-router';
 import * as SecureStore from 'expo-secure-store';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
@@ -48,6 +48,7 @@ const InitialLayout = () => {
 
   const router = useRouter();
   const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -62,10 +63,21 @@ const InitialLayout = () => {
 
   useEffect(() => {
     console.log('isSignedIn', isSignedIn);
+    if (!isLoaded) return;
+
+    // Check whether user is in (authenticated) file group
+    const inAuthGroup = segments[0] === '(authenticated)';
+
+    // If user is signed in but not in the authenticated group, push them there
+    if (isSignedIn && !inAuthGroup) {
+      router.replace('/(authenticated)/(tabs)/home');
+    } else if (!isSignedIn) {
+      router.replace('/');
+    }
   }, [isSignedIn]);
 
-  if (!loaded) {
-    return null;
+  if (!loaded || !isLoaded) {
+    return <Text>Loading...</Text>;
   }
 
   return (
