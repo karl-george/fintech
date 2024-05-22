aa231a14baf1f687c884e0522dec29a913e0904c
Karl George <karlgeorge87@gmail.com>
2024-05-22 11:32:56 +0200

    Add authenticated tabs

---
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import { Tabs } from 'expo-router';
+
+const Layout = () => {
+  return (
+    <Tabs>
+      <Text>layout</Text>
+    </Tabs>
+  );
+};
+
+export default Layout;
---
+++ app/(authenticated)/(tabs)/invest.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from 'react-native';
+
+const Page = () => {
+  return <View></View>;
+};
+
+export default Page;
