3c0b27fb96645eb5dcea53c30a17195543eb5d65
Karl George <karlgeorge87@gmail.com>
2024-05-22 13:19:37 +0200

    Style main page

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -1,7 +1,54 @@
-import { View, Text } from 'react-native';
+import RoundButton from '@/components/RoundButton';
+import Colors from '@/constants/Colors';
+import { View, Text, ScrollView, StyleSheet, Button } from 'react-native';
 
 const Page = () => {
-  return <View></View>;
+  const balance = 1420;
+
+  const onAddMoney = () => {};
+
+  return (
+    <ScrollView style={{ backgroundColor: Colors.background }}>
+      <View style={styles.account}>
+        <View style={styles.row}>
+          <Text style={styles.balance}>{balance}</Text>
+          <Text style={styles.currency}>€</Text>
+        </View>
+      </View>
+      <View style={styles.actionRow}>
+        <RoundButton icon={'add'} text='Add money' onPress={onAddMoney} />
+        <RoundButton icon={'refresh'} text='Exchange' />
+        <RoundButton icon={'list'} text='Details'/>
+        <RoundButton icon={'add'} text='Add money' onPress={onAddMoney} />
+      </View>
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  account: {
+    margin: 80,
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  balance: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  currency: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  actionRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+});
 
 export default Page;
