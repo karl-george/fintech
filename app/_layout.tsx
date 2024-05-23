f7aaa807633babb6946fa2da1490f573f02945e8
Karl George <karlgeorge87@gmail.com>
2024-05-23 11:48:52 +0200

    Add tanstack provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,8 +11,11 @@ import { useEffect } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const queryClient = new QueryClient();
 
 // Cache the Clerk JWT for secure storage
 const tokenCache = {
@@ -154,10 +157,12 @@ const RootLayoutNav = () => {
       publishableKey={CLERK_PUBLISHABLE_KEY!}
       tokenCache={tokenCache}
     >
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <StatusBar style='light' />
-        <InitialLayout />
-      </GestureHandlerRootView>
+      <QueryClientProvider client={queryClient}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <StatusBar style='light' />
+          <InitialLayout />
+        </GestureHandlerRootView>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 };
