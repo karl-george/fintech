2efbda081d398248108a4faf9685c8d42db6b11d
Karl George <karlgeorge87@gmail.com>
2024-05-23 13:38:25 +0200

    Style header for crypto details page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,7 @@ import * as SecureStore from 'expo-secure-store';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -146,6 +146,33 @@ const InitialLayout = () => {
       <Stack.Screen
         name='(authenticated)/(tabs)'
         options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='(authenticated)/crypto/[id]'
+        options={{
+          title: '',
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name='arrow-back' size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+          headerLargeTitle: true,
+          headerTransparent: true,
+          headerRight: () => (
+            <View style={{ flexDirection: 'row', gap: 10 }}>
+              <TouchableOpacity>
+                <Ionicons
+                  name='notifications-outline'
+                  size={30}
+                  color={Colors.dark}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Ionicons name='star-outline' size={30} color={Colors.dark} />
+              </TouchableOpacity>
+            </View>
+          ),
+        }}
       />
     </Stack>
   );
