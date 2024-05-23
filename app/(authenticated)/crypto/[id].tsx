21d082c24eb3e6d942c098b405d9136fd644c54e
Karl George <karlgeorge87@gmail.com>
2024-05-23 14:03:23 +0200

    Style details page

--- app/(authenticated)/crypto/[id].tsx
+++ app/(authenticated)/crypto/[id].tsx
@@ -1,11 +1,75 @@
-import { View, Text } from 'react-native';
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { useHeaderHeight } from '@react-navigation/elements';
+import { useQuery } from '@tanstack/react-query';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import { Image, SectionList, StyleSheet, Text, View } from 'react-native';
 
 const Page = () => {
+  const { id } = useLocalSearchParams();
+  const headerHeight = useHeaderHeight();
+
+  const { data } = useQuery({
+    queryKey: ['info', id],
+    queryFn: async () => {
+      const info = await fetch(`/api/info?ids=${id}`).then((res) => res.json());
+      return info[+id!];
+    },
+  });
+
   return (
-    <View>
-      <Text>Details</Text>
-    </View>
+    <>
+      <Stack.Screen options={{ title: data?.name }} />
+      <SectionList
+        style={{ paddingTop: headerHeight }}
+        contentInsetAdjustmentBehavior='automatic'
+        keyExtractor={(item) => item.title}
+        sections={[{ data: [{ title: 'Chart' }] }]}
+        ListHeaderComponent={() => (
+          <>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                marginHorizontal: 16,
+              }}
+            >
+              <Text style={styles.subtitle}>{data?.symbol}</Text>
+              <Image
+                source={{ uri: data?.logo }}
+                style={{ width: 60, height: 60 }}
+              />
+            </View>
+          </>
+        )}
+        renderItem={({ item }) => (
+          <>
+            <View style={[defaultStyles.block, { marginTop: 20 }]}>
+              <Text style={styles.subtitle}>Overview</Text>
+              <Text style={{ color: Colors.gray }}>
+                Bitcoin is a decentralized digital currency, without a central
+                bank or single administrator, that can be sent from user to user
+                on the peer-to-peer bitcoin network without the need for
+                intermediaries. Transactions are verified by network nodes
+                through cryptography and recorded in a public distributed ledger
+                called a blockchain.
+              </Text>
+            </View>
+          </>
+        )}
+      ></SectionList>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  subtitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: Colors.gray,
+  },
+});
 
 export default Page;
