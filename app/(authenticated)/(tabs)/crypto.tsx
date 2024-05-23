0876fc9756bb2f3b28fa1e764f7c0bae0d1b1cad
Karl George <karlgeorge87@gmail.com>
2024-05-23 12:00:48 +0200

    Retrieve data from the api

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,17 +1,33 @@
 import { Currency } from '@/interfaces/crypto';
 import { useQuery } from '@tanstack/react-query';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 const Page = () => {
   const currencies = useQuery({
-    queryKey: ['currencies'],
+    queryKey: ['listings'],
     queryFn: () => fetch('/api/listings').then((res) => res.json()),
+  });
+
+  const ids = currencies.data
+    ?.map((currency: Currency) => currency.id)
+    .join(',');
+
+  const { data } = useQuery({
+    queryKey: ['info', ids],
+    queryFn: () => fetch(`/api/info?ids=${ids}`).then((res) => res.json()),
+    enabled: !!ids,
   });
 
   return (
     <View>
       {currencies.data?.map((currency: Currency) => (
-        <Text key={currency.id}>{currency.name}</Text>
+        <View style={{ flexDirection: 'row' }} key={currency.id}>
+          <Image
+            source={{ uri: data?.[currency.id].logo }}
+            style={{ width: 32, height: 32 }}
+          />
+          <Text>{currency.name}</Text>
+        </View>
       ))}
     </View>
   );
