b8b62f5642c2087ec66a455067ca876217e337c8
Karl George <karlgeorge87@gmail.com>
2024-05-23 11:48:59 +0200

    Retrieve currencies

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,10 +1,20 @@
-import { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { Currency } from '@/interfaces/crypto';
+import { useQuery } from '@tanstack/react-query';
+import { Text, View } from 'react-native';
 
 const Page = () => {
-  useEffect(() => {}, []);
+  const currencies = useQuery({
+    queryKey: ['currencies'],
+    queryFn: () => fetch('/api/listings').then((res) => res.json()),
+  });
 
-  return <View></View>;
+  return (
+    <View>
+      {currencies.data?.map((currency: Currency) => (
+        <Text key={currency.id}>{currency.name}</Text>
+      ))}
+    </View>
+  );
 };
 
 export default Page;
