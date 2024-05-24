0df10631594a17f17095c959f0112e4ab8793420
Karl George <karlgeorge87@gmail.com>
2024-05-24 13:34:29 +0200

    Implement lock screen

--- app/(authenticated)/(modals)/lock.tsx
+++ app/(authenticated)/(modals)/lock.tsx
@@ -1,11 +1,175 @@
-import { View, Text } from 'react-native';
+import Colors from '@/constants/Colors';
+import { useUser } from '@clerk/clerk-expo';
+import { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import * as Haptics from 'expo-haptics';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 const Page = () => {
+  const { user } = useUser();
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [code, setCode] = useState<number[]>([]);
+  const codeLength = Array(6).fill(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (code.length === 6) {
+      if (code.join('') === '111111') {
+        router.replace('/(authenticated)/(tabs)/home');
+        setCode([]);
+      } else {
+        // TODO: Error
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+        setCode([]);
+      }
+    }
+  }, [code]);
+
+  const onNumberPress = (number: number) => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    setCode([...code, number]);
+  };
+
+  const numberBackspace = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    setCode(code.slice(0, -1));
+  };
+
+  const onBiometricAuthPress = async () => {
+    const { success } = await LocalAuthentication.authenticateAsync();
+    if (success) {
+      router.replace('/(authenticated)/(tabs)/home');
+    } else {
+      Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+    }
+  };
+
   return (
-    <View>
-      <Text>lock</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={styles.greeting}>Welcome back, {firstName}</Text>
+      <View style={[styles.codeView]}>
+        {codeLength.map((_, index) => (
+          <View
+            key={index}
+            style={[
+              styles.codeEmpty,
+              {
+                backgroundColor: code[index]
+                  ? Colors.primary
+                  : Colors.lightGray,
+              },
+            ]}
+          />
+        ))}
+      </View>
+
+      <View style={styles.numbersView}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          {[1, 2, 3].map((number) => (
+            <TouchableOpacity
+              key={number}
+              onPress={() => onNumberPress(number)}
+            >
+              <Text style={styles.number}>{number}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          {[4, 5, 6].map((number) => (
+            <TouchableOpacity
+              key={number}
+              onPress={() => onNumberPress(number)}
+            >
+              <Text style={styles.number}>{number}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          {[7, 8, 9].map((number) => (
+            <TouchableOpacity
+              key={number}
+              onPress={() => onNumberPress(number)}
+            >
+              <Text style={styles.number}>{number}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <TouchableOpacity onPress={onBiometricAuthPress}>
+            <MaterialCommunityIcons
+              name='face-recognition'
+              size={26}
+              color={'black'}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => onNumberPress(0)}>
+            <Text style={styles.number}>0</Text>
+          </TouchableOpacity>
+          <View style={{ minWidth: 30 }}>
+            {code.length > 0 && (
+              <TouchableOpacity onPress={numberBackspace}>
+                <MaterialCommunityIcons
+                  name='backspace-outline'
+                  color={'black'}
+                  size={26}
+                />
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
+        <Text
+          style={{
+            alignSelf: 'center',
+            color: Colors.primary,
+            fontWeight: '500',
+            fontSize: 18,
+          }}
+        >
+          Forgot your passcode?
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
-
+const styles = StyleSheet.create({
+  greeting: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 80,
+    alignSelf: 'center',
+  },
+  codeView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 20,
+    marginVertical: 100,
+  },
+  codeEmpty: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+  },
+  numbersView: {
+    marginHorizontal: 80,
+    gap: 60,
+  },
+  number: {
+    fontSize: 32,
+  },
+});
 export default Page;
